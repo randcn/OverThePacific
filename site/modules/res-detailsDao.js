b86b9c0dadbb0215c94fc967bc78ce7be2a40a9d
yan.liu <yl732@students.waikato.ac.nz>
2021-09-09 02:22:15 +0800

    add submit and rate

--- site/modules/res-detailsDao.js
+++ site/modules/res-detailsDao.js
@@ -8,12 +8,20 @@ async function getResDetails(business_id) {
 
 async function getAllReviewsForRes(business_id) {
     const db = await database;
-    const reviews = await db.query( "select a.name, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id="+business_id+"");
+    const reviews = await db.query( "select a.name, a.user_id, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id="+business_id+"");
     return reviews;
+}
+
+async function insertReview(user_id, business_id, stars, text) {
+    const db = await database;
+    let result;
+    result = await db.query( "insert into reviews (user_id, business_id, stars, text) VALUES ("+user_id+","+business_id+", "+stars+", "+text+")");
+    return result;
 }
 
 
 module.exports = {
     getAllReviewsForRes,
-    getResDetails
+    getResDetails,
+    insertReview
 };
--- site/routes/res-details.js
+++ site/routes/res-details.js
@@ -7,11 +7,11 @@ const dao = require("../modules/res-detailsDao.js");
 router.get("/details", async function (req, res) {
 
     res.locals.title = "Details";
-
     //get restaurant name and all comment from DB:
     let reviews;
     let reviewsNum;
     let restaurant;
+
     restaurant = await dao.getResDetails(1);
     reviews = await dao.getAllReviewsForRes(1);
     reviewsNum = reviews.length;
@@ -19,8 +19,24 @@ router.get("/details", async function (req, res) {
     res.locals.restaurant = restaurant[0];
     res.locals.reviews = reviews;
     res.locals.reviewsNum = reviewsNum;
+    for (let i=0; i<reviews.length;i++){
+        res.locals.reviews[i].date = reviews[i].date.toString().substring(0,24);
+    }
+
 
     res.render("details");
 });
+
+router.post("/insertReview", async function(req, res) {
+    let reviewText = "'"+req.body.reviewText+"'";
+    let star = req.body.star;
+    let business_id = req.body.business_id;
+    let user_id = "'" +req.session.user.user_id+"'";
+    let result;
+    result = await dao.insertReview(user_id, business_id, star, reviewText);
+
+    res.send(result);
+});
+
 
 module.exports = router;
