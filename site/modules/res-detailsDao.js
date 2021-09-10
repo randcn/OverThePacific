89fb4b9bd6f499315468e0b54b8f22b2775e2c90
yan.liu <yl732@students.waikato.ac.nz>
2021-09-11 02:04:39 +0800

    add stars/review_count to restaurants, add view more function, add page redirect

--- site/modules/res-detailsDao.js
+++ site/modules/res-detailsDao.js
@@ -8,7 +8,7 @@ async function getResDetails(business_id) {
 
 async function getAllReviewsForRes(business_id) {
     const db = await database;
-    const reviews = await db.query( "select a.name, a.user_id,b.stars, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id="+business_id+"");
+    const reviews = await db.query( "select a.name, a.user_id,b.stars, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id="+business_id+" ORDER BY b.date DESC");
     return reviews;
 }
 
@@ -19,9 +19,25 @@ async function insertReview(user_id, business_id, stars, text) {
     return result;
 }
 
+async function updateRestaurants(business_id, stars) {
+    const db = await database;
+    let result;
+    let review_count;
+    let starsBeforeInsert = await db.query( "select stars from restaurants where business_id= "+business_id+"");
+    review_count = await db.query( "select review_count from restaurants where business_id= "+business_id+"");
+    let newReview_count = parseInt(review_count[0].review_count) +1;
+    starsBeforeInsert = starsBeforeInsert[0].stars;
+    let addedStars = stars/newReview_count;
+    let newStars= parseFloat(starsBeforeInsert) +  parseFloat(addedStars);
+    newStars = newStars.toFixed(8)
+    result = await db.query( "update restaurants set stars = "+newStars+", review_count = "+newReview_count+" where business_id= "+business_id+"");
+    return result;
+}
+
 
 module.exports = {
     getAllReviewsForRes,
     getResDetails,
-    insertReview
+    insertReview,
+    updateRestaurants
 };
