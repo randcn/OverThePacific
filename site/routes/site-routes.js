cbf7166acfa65d9b760d22eb953425cd1422a74d
yan.liu <yl732@students.waikato.ac.nz>
2021-09-15 14:33:49 +0800

    restaurants.stars length

--- site/routes/site-routes.js
+++ site/routes/site-routes.js
@@ -17,7 +17,11 @@ router.get("/", async function(req,res){
     const restaurants = await restaurantsDAO.getAllRestaurants();
     //console.log(restaurants)
     res.locals.restaurants = restaurants;
-    
+
+    for (let i=0; i<restaurants.length; i++) {
+        res.locals.restaurants[i].stars = restaurants[i].stars.toString().substring(0,3);
+    }
+
     res.render("main");
 });
 
