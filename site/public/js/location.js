cd4718c61c20752b5a76ad0e79f30b03169d8e2b
yan.liu <yl732@students.waikato.ac.nz>
2021-09-17 07:55:42 +0800

    add display number - 10 restaurants per page.

--- site/public/js/location.js
+++ site/public/js/location.js
@@ -5,25 +5,37 @@ function selectOnchange() {
     const cityIndex = citySelect.selectedIndex;
     const city = citySelect.options[cityIndex].value;
     const restaurantsDiv = document.querySelector("#restaurants_container");
-    restaurants = document.querySelectorAll(".restaurant_card");
-    viewMore = document.querySelector(".viewMore");
+
 
         console.log(city);
         $.ajax({
-            async: false,
             type: "POST",
             url: "/city",
             data: {city: city},
             success: function (data) {
                 if (data == "1") {
                     $("#restaurants_container").load(location.href + " #restaurants_container");
-                    setOriginalDisplay();
-                    if (viewMore!=null) {
-                        viewMore.addEventListener("click", () => {
-                            viewMoreItems();
-                        });
-                    }
                 }
             }
         })
+
+    let sleep = function(time){
+        setTimeout(()=>{
+
+            displayRestaurant();
+        },time);
+    }
+
+    sleep(1000);
+
+    function displayRestaurant() {
+        restaurants = document.querySelectorAll(".restaurant_card");
+        viewMore = document.querySelector(".viewMore");
+        setOriginalDisplay();
+        if (viewMore != null) {
+            viewMore.addEventListener("click", () => {
+                viewMoreItems();
+            });
+        }
+    }
 }
