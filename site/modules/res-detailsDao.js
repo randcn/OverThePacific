4074e58268d578890af3f162cebb84ab00af506f
Xiaojuan Li <xl370@students.waikato.ac.nz>
2021-09-27 11:34:17 +1300

    fixed bug  of main page to detail page

--- site/modules/res-detailsDao.js
+++ site/modules/res-detailsDao.js
@@ -2,20 +2,20 @@ const database = require("./database.js");
 
 async function getResDetails(business_id) {
     const db = await database;
-    const name = await db.query( "select * from restaurants where business_id ="+business_id+"");
+    const name = await db.query( "select * from restaurants where business_id = ?", [business_id]);
     return name;
 }
 
 async function getAllReviewsForRes(business_id) {
     const db = await database;
-    const reviews = await db.query( "select a.name, a.user_id,b.stars, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id="+business_id+" ORDER BY b.date DESC");
+    const reviews = await db.query( "select a.name, a.user_id,b.stars, b.text, b.business_id, b.date from users as a, reviews as b where a.user_id=b.user_id and b.business_id=? ORDER BY b.date DESC",[business_id]);
     return reviews;
 }
 
 async function insertReview(user_id, business_id, stars, text) {
     const db = await database;
     let result;
-    result = await db.query( "insert into reviews (user_id, business_id, stars, text) VALUES ("+user_id+","+business_id+", "+stars+", "+text+")");
+    result = await db.query( "insert into reviews (user_id, business_id, stars, text) VALUES (?,?, ?, ?)",[user_id],[business_id],[stars],[text]);
     return result;
 }
 
@@ -23,8 +23,8 @@ async function updateRestaurants(business_id, stars) {
     const db = await database;
     let result;
     let review_count;
-    let starsBeforeInsert = await db.query( "select stars from restaurants where business_id= "+business_id+"");
-    review_count = await db.query( "select review_count from restaurants where business_id= "+business_id+"");
+    let starsBeforeInsert = await db.query( "select stars from restaurants where business_id= ?",[business_id]);
+    review_count = await db.query( "select review_count from restaurants where business_id= ?",[business_id]);
     let newReview_count = parseInt(review_count[0].review_count) +1;
     starsBeforeInsert = starsBeforeInsert[0].stars;
     let addedStars = stars/newReview_count;
